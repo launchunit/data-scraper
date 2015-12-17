dcdf8bda66ad1a6a30e92ce372ed920445aee393
Karan Sakhuja <kevin.sakhuja@gmail.com>
2015-12-17 13:08:43 -0500

    Cleanup of things

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-exports.getData = require('./lib/get');
+/**
+ * GET JSON from API
+ *
+ * @param opts.seedData {Object|Array} - [{url,query,meta}]
+ * @param opts.requestType {String} 'json|html'
+ * @param opts.resultFn {Function} // Async
+ * @param opts.nextSeedFn {Function}
+ *        @return {Object|Array} [{url,query,meta}], // null can also be passed
+ * @param opts.headers {Object} - GET request headers
+ * @param opts.query {Object} - GET request query
+ * @param opts.concurrency (Optional) Default = 10
+ **/
+ exports.getData = require('./lib/get');
